fa64edd8a4aa67297ebca9dd47747c16a952b1da
Ben Keung <benkeungg@gmail.com>
2014-10-17 23:15:26 -0700

    Added a stub file for tests

---
+++ analyzer/tests.py
@@ -0,0 +1,17 @@
+import unittest
+
+class CodeComplexityTestCase(unittest.TestCase):
+
+    def setUp(self):
+        """Setup"""
+        pass
+
+    def tearDown(self):
+        """Teardown."""
+        pass
+
+    def testtest(self):
+        assert True
+
+if __name__ == "__main__":
+    unittest.main()
