734a7a3db8b6d5739dd4a76adefdc03d6e2ee034
Gurkaran Poonia <gurkaran.poonia@gmail.com>
2014-10-26 14:37:13 -0700

    Some stub tests

--- ui/tests/tests.js
+++ ui/tests/tests.js
@@ -1,6 +1,12 @@
 /**
  * Created by gurkaran on 2014-10-26.
  */
-QUnit.test( "Init Test", function( assert ) {
-    assert.ok( 1 == "1", "Passed!" );
+QUnit.test( "Test Mock JSON Parsing", function( assert ) {
+
+    assert.ok( 1 == "2", "TODO" );
+});
+
+QUnit.test( "Test Bubble Object Creation", function( assert ) {
+
+    assert.ok( 1 == "2", "TODO" );
 });
